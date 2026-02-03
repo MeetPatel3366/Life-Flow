778bdcf470b725ed3cfa36c9377a17b73f6ecd5e
MeetPatel3366 <meet3366patel@gmail.com>
2026-02-03 15:31:52 +0530

    feat: implement connect db & server.js

---
+++ server/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const { connection } = await mongoose.connect(process.env.MONGO_URI);
+
+    if (connection) {
+      console.log(`Connected to MongoDB: ${connection.host}`);
+    }
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
