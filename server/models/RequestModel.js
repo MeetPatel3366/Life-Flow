fef45a0a3d7c12d2f6df57566ab2f6a30e962dec
MeetPatel3366 <meet3366patel@gmail.com>
2026-02-04 14:26:41 +0530

    feat: implement Request schema & model

---
+++ server/models/RequestModel.js
@@ -0,0 +1,87 @@
+import mongoose from "mongoose";
+
+const requestSchema = new mongoose.Schema(
+  {
+    patient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    hospital: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Hospital",
+      required: true,
+    },
+    bloodGroup: {
+      type: String,
+      enum: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+      required: true,
+    },
+    componentType: {
+      type: String,
+      enum: ["Whole Blood", "RBC", "Plasma", "Platelets"],
+      required: true,
+    },
+    unitsRequired: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+    urgency: {
+      type: String,
+      enum: [
+        "Normal", //Planned need
+        "Urgent", //Needed soon
+        "Emergency", //Accident, ICU bleeding
+      ],
+      default: "Normal",
+    },
+    requiredDate: {
+      type: Date,
+    },
+    diagnosis: {
+      type: String,
+    },
+    status: {
+      type: String,
+      enum: [
+        "Pending", // waiting admin review
+        "Approved", // approved, stock reserved
+        "Rejected", // denied
+        "Awaiting Donor", // no stock, donor alert sent
+        "Transfer Required", // stock in other center
+        "Ready for Issue", // blood prepared
+        "Completed", // blood issued
+        "Cancelled",
+      ],
+      default: "Pending",
+    },
+    approvedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    approvalDate: {
+      type: Date,
+    },
+    rejectionReason: {
+      type: String,
+    },
+    bloodUnits: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "BloodStock",
+      },
+    ],
+    transfer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Transfer",
+    },
+    notes: {
+      type: String,
+      trim: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("Request", requestSchema);
