b3c54873e2a0dbab603944ebfa775f43dcb86cd7
MeetPatel3366 <meet3366patel@gmail.com>
2026-02-04 17:02:27 +0530

    feat: implement global error handling middleware

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { config } from "dotenv";
+import { errorMiddleware } from "./middlewares/error.middleware";
 
 config();
 
@@ -9,5 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use(errorMiddleware);
 
 export default app;
