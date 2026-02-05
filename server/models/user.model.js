c90f1048e7d3b6e1aef8fe4a5f33499a2f8aaaef
MeetPatel3366 <meet3366patel@gmail.com>
2026-02-05 14:02:05 +0530

    feat: implement password hashing and verification methods in User model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
@@ -88,5 +89,18 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true },
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 export default mongoose.model("User", userSchema);
