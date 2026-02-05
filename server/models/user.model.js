be025593ce133f2aacd47914a8ec9277f41e2ba5
MeetPatel3366 <meet3366patel@gmail.com>
2026-02-05 14:23:22 +0530

    feat: add JWT generation methods to User model & add refesh token field and profileImage field

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
   {
@@ -82,9 +83,20 @@ const userSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "Hospital",
     },
+    refreshToken: {
+      type: String,
+    },
     isActive: {
       type: Boolean,
       default: true,
+    },
+    profileImage: {
+      public_id: {
+        type: String,
+      },
+      secure_url: {
+        type: String,
+      },
     },
   },
   { timestamps: true },
@@ -95,12 +107,38 @@ userSchema.pre("save", async function (next) {
     return next();
   }
 
-  this.password = bcrypt.hash(this.password, 10);
+  this.password =await bcrypt.hash(this.password, 10);
   next();
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+      email: this.email,
+      name: this.name,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    },
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    },
+  );
 };
 
 export default mongoose.model("User", userSchema);
