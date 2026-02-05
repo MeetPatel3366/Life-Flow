3c8c92a45cc02c42030a22286e70d585275f4e44
MeetPatel3366 <meet3366patel@gmail.com>
2026-02-05 11:24:05 +0530

    feat: add ApiError class and centralized error handling structure

--- server/middlewares/error.middleware.js
+++ server/middlewares/error.middleware.js
@@ -1,18 +1,31 @@
+import { ApiError } from "../utils/ApiError";
+
 export const notFound = (req, res, next) => {
-  const error = new Error(`Not Found - ${req.originalUrl}`);
-  res.status(404);
-  next(error);
+  next(new ApiError(404, `Not Found - ${req.originalUrl}`));
 };
 
+import { ApiError } from "../utils/ApiError.js";
+
 export const errorMiddleware = (err, req, res, next) => {
-  console.error(err.stack);
+  let error = err;
 
-  const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
+  if (!(error instanceof ApiError)) {
+    error = new ApiError(
+      error.statusCode || 500,
+      error.message || "Internal Server Error",
+      [],
+      error.stack,
+    );
+  }
 
-  res.status(statusCode).json({
-    success: false,
-    message: message,
-    stack: process.env.MODE == "development" ? err.stack : null,
+  if (process.env.NODE_ENV === "development") {
+    console.error(error.stack);
+  }
+
+  res.status(error.statusCode).json({
+    success: error.success,
+    message: error.message,
+    errors: error.errors,
+    stack: process.env.NODE_ENV === "development" ? error.stack : undefined,
   });
 };
