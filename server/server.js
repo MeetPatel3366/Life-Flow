f160342d86079dbd37d46f4c73748fdcb0f01ada
MeetPatel3366 <meet3366patel@gmail.com>
2026-02-05 09:51:05 +0530

    feat: refactor server startup to ensure DB connection before listening

--- server/server.js
+++ server/server.js
@@ -3,7 +3,17 @@ import connectDB from "./config/db.js";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, async () => {
-  await connectDB();
-  console.log(`Server running at PORT : ${PORT}`);
-});
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Server Error : ", error);
+      throw error;
+    });
+    app.listen(PORT, async () => {
+      console.log(`Server running at PORT : ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed ", err);
+    process.exit(1);
+  });
